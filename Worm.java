a8c10e3c413e585fda225391febea62fb54ad785
brittneyhung <thung25@sandomenico.org>
2024-10-01 19:13:43 -0700

    Update Worm.java

--- Worm.java
+++ Worm.java
@@ -4,8 +4,8 @@ public class Worm extends Actor
 {
     public void act()
     {
-        move(2);      // Move the worm forward at a speed of 2
-        turnAtEdge(); // Turn when reaching the edge of the screen
+        move(2);      
+        turnAtEdge(); 
     }
 
     /**
@@ -14,7 +14,7 @@ public class Worm extends Actor
     public void turnAtEdge()
     {
         if (isAtEdge()) {
-            turn(Greenfoot.getRandomNumber(90) - 45);  // Turn in a random direction between -45 to 45 degrees
+            turn(Greenfoot.getRandomNumber(90) - 45);  
         }
     }
 }
